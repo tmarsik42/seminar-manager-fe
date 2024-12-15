708f6353f18fe31b331a76035e66736f5ae1a414
Tadeáš Maršík <tadeas.marsik@teta.eu>
2024-12-15 12:27:32 +0100

    feat: Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    const { text, icon, onclick }: { text: string; icon: string; onclick: () => void } = $props();
+</script>
+
+<style lang="scss">
+    button {
+        background-color: black;
+        color: white;
+        border-radius: 16px;
+        padding: var(--tdk-spacing-sm) var(--tdk-spacing-md);
+        cursor: pointer;
+
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+</style>
+
+<button onclick={onclick}>
+    {text}
+    {icon}
+</button>
